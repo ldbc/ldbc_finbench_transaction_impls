0de8ef8b570d9a2fed7822e87336e57d935cc4f0
qishipengqsp <qishipeng.qsp@antgroup.com>
2023-02-22 10:43:11 +0800

    wip

--- tugraph/src/main/java/org/ldbcouncil/finbench/impls/tugraph/CustomDataOutputStream.java
+++ tugraph/src/main/java/org/ldbcouncil/finbench/impls/tugraph/CustomDataOutputStream.java
@@ -3,44 +3,48 @@ package org.ldbcouncil.finbench.impls.tugraph;
 import java.io.ByteArrayOutputStream;
 
 public class CustomDataOutputStream {
-	
-	private ByteArrayOutputStream out;
-	
-	public CustomDataOutputStream() {
-		out = new ByteArrayOutputStream();
-	}
-	
-	public void writeInt16(short i) {
-		out.write((i     ) & 0xff);
-		out.write((i >> 8) & 0xff);
-	}
 
-	public void writeInt32(int i) {
-		out.write((i      ) & 0xff);
-		out.write((i >>  8) & 0xff);
-		out.write((i >> 16) & 0xff);
-		out.write((i >> 24) & 0xff);
-	}
+    private ByteArrayOutputStream out;
 
-	public void writeInt64(long i) {
-		out.write((int)((i      ) & 0xff));
-		out.write((int)((i >>  8) & 0xff));
-		out.write((int)((i >> 16) & 0xff));
-		out.write((int)((i >> 24) & 0xff));
-		out.write((int)((i >> 32) & 0xff));
-		out.write((int)((i >> 40) & 0xff));
-		out.write((int)((i >> 48) & 0xff));
-		out.write((int)((i >> 56) & 0xff));
-	}
+    public CustomDataOutputStream() {
+        out = new ByteArrayOutputStream();
+    }
 
-	public void writeString(String s) {
-		int sLen = s.getBytes().length;
-		writeInt16((short)sLen);
-		out.write(s.getBytes(), 0, sLen);
-	}
-	
-	public byte[] toByteArray() {
-		return out.toByteArray();
-	}
+    public void writeByte(Byte i) {
+        out.write((i) & 0xff);
+    }
+
+    public void writeInt16(short i) {
+        out.write((i) & 0xff);
+        out.write((i >> 8) & 0xff);
+    }
+
+    public void writeInt32(int i) {
+        out.write((i) & 0xff);
+        out.write((i >> 8) & 0xff);
+        out.write((i >> 16) & 0xff);
+        out.write((i >> 24) & 0xff);
+    }
+
+    public void writeInt64(long i) {
+        out.write((int) ((i) & 0xff));
+        out.write((int) ((i >> 8) & 0xff));
+        out.write((int) ((i >> 16) & 0xff));
+        out.write((int) ((i >> 24) & 0xff));
+        out.write((int) ((i >> 32) & 0xff));
+        out.write((int) ((i >> 40) & 0xff));
+        out.write((int) ((i >> 48) & 0xff));
+        out.write((int) ((i >> 56) & 0xff));
+    }
+
+    public void writeString(String s) {
+        int sLen = s.getBytes().length;
+        writeInt16((short) sLen);
+        out.write(s.getBytes(), 0, sLen);
+    }
+
+    public byte[] toByteArray() {
+        return out.toByteArray();
+    }
 
 }
--- tugraph/src/main/java/org/ldbcouncil/finbench/impls/tugraph/TuGraphDbConnectionState.java
+++ tugraph/src/main/java/org/ldbcouncil/finbench/impls/tugraph/TuGraphDbConnectionState.java
@@ -1,7 +1,7 @@
 package org.ldbcouncil.finbench.impls.tugraph;
 
-import org.ldbcouncil.finbench.driver.DbConnectionState;
 import com.antgroup.tugraph.TuGraphRpcClient;
+import org.ldbcouncil.finbench.driver.DbConnectionState;
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -9,38 +9,37 @@ import java.util.Map;
 
 public class TuGraphDbConnectionState extends DbConnectionState {
 
-	private String uri;
-	private String user;
-	private String pass;
-	private LinkedList<TuGraphRpcClient> clientPool;
-	private TuGraphRpcClient client;
+    private String uri;
+    private String user;
+    private String pass;
+    private LinkedList<TuGraphRpcClient> clientPool;
+    private TuGraphRpcClient client;
 
-	public TuGraphDbConnectionState(Map<String, String> properties) throws IOException {
-		uri = properties.get("uri");
+    public TuGraphDbConnectionState(Map<String, String> properties) throws IOException {
+        uri = properties.get("uri");
+        user = properties.get("user");
+        pass = properties.get("pass");
+        clientPool = new LinkedList<>();
+        client = new TuGraphRpcClient(uri, user, pass);
+    }
 
-		user = properties.get("user");
-		pass = properties.get("pass");
-		clientPool = new LinkedList<>();
-		client = new TuGraphRpcClient(uri, user, pass);
-	}
+    public synchronized TuGraphRpcClient popClient() throws IOException {
+        if (clientPool.isEmpty()) {
+            clientPool.add(new TuGraphRpcClient(uri, user, pass));
+        }
+        return clientPool.pop();
+    }
 
-	public synchronized TuGraphRpcClient popClient() throws IOException {
-		if (clientPool.isEmpty()) {
-			clientPool.add(new TuGraphRpcClient(uri, user, pass));
-		}
-		return clientPool.pop();
-	}
+    public synchronized void pushClient(TuGraphRpcClient client) {
+        clientPool.push(client);
+    }
 
-	public synchronized void pushClient(TuGraphRpcClient client) {
-		clientPool.push(client);
-	}
-
-	@Override
-	public void close() throws IOException {
-		while (!clientPool.isEmpty()) {
-			clientPool.pop();
-		}
-		client.logout();
-	}
+    @Override
+    public void close() throws IOException {
+        while (!clientPool.isEmpty()) {
+            clientPool.pop();
+        }
+        client.logout();
+    }
 
 }
