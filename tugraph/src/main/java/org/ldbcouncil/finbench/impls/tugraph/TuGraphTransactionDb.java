44b1cbc97cce7e03747e7ecb3180ef6103ee8924
kehuang <hk286513@antgroup.com>
2023-05-28 20:17:30 +0800

    update tcr6

--- tugraph/src/main/java/org/ldbcouncil/finbench/impls/tugraph/TuGraphTransactionDb.java
+++ tugraph/src/main/java/org/ldbcouncil/finbench/impls/tugraph/TuGraphTransactionDb.java
@@ -271,7 +271,7 @@ public class TuGraphTransactionDb extends Db {
                 ResultReporter resultReporter) throws DbException {
             try {
                 TuGraphDbRpcClient client = dbConnectionState.popClient();
-                String cypher = "MATCH (dstCard:Account {id:%d} )<-[edge2:withdraw]-(mid:Account) WHERE dstCard.type = 'tp2' AND edge2.timestamp > %d AND edge2.timestamp < %d AND edge2.amount > %f WITH mid, sum(edge2.amount) as sumEdge2Amount, count(edge2.amount) as t MATCH (mid)<-[edge1:transfer]-(src:Account) WHERE edge1.timestamp > %d AND edge1.timestamp < %d AND edge1.amount > %f WITH mid.id AS midId, count(edge1) AS edge1Count, sum(edge1.amount) AS sumEdge1Amount, sumEdge2Amount WHERE edge1Count > 3 RETURN midId, sumEdge1Amount, sumEdge2Amount ORDER BY sumEdge2Amount DESC;";
+                String cypher = "MATCH (dstCard:Account {id:%d} )<-[edge2:withdraw]-(mid:Account) WHERE dstCard.type ENDS WITH 'card' AND edge2.timestamp > %d AND edge2.timestamp < %d AND edge2.amount > %f WITH mid, sum(edge2.amount) as sumEdge2Amount, count(edge2.amount) as t MATCH (mid)<-[edge1:transfer]-(src:Account) WHERE edge1.timestamp > %d AND edge1.timestamp < %d AND edge1.amount > %f WITH mid.id AS midId, count(edge1) AS edge1Count, sum(edge1.amount) AS sumEdge1Amount, sumEdge2Amount WHERE edge1Count > 3 RETURN midId, sumEdge1Amount, sumEdge2Amount ORDER BY sumEdge2Amount DESC;";
                 long startTime = cr6.getStartTime().getTime();
                 long endTime = cr6.getEndTime().getTime();
                 double threshold1 = cr6.getThreshold1();
